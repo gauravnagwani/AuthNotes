5a0e0055a3d3ebcb1aa463d055a1359d03da0336
Gaurav Nagwani <gauravnagwani859@gmail.com>
2025-09-04 15:39:51 +0530

    Added middleware and api route

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
+import userRoute from "./routes/userRoute.js";
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT ||3000
+
+//middleware
+app.use(express.json());
+
+//api routes
+app.use("/user",userRoute)
+//http://localhost:8000/user/register
+
+
 app.listen(PORT,()=>{
     connectDB()
     console.log(`Server is running on port ${PORT}`);
