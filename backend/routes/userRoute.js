c4ae1a7cd5a07c5d28345279f84ca929770c1a2a
Gaurav Nagwani <gauravnagwani859@gmail.com>
2025-09-07 15:01:20 +0530

    Added route forgot-password

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { loginUser, logoutUser, registerUser, verification } from "../controllers/userController.js";
+import { forgotPassword, loginUser, logoutUser, registerUser, verification } from "../controllers/userController.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.js";
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.post("/register",registerUser)
 router.post("/verify",verification)
 router.post("/login",loginUser)
 router.post("/logout" , isAuthenticated, logoutUser)
+router.post("/forgot-password",forgotPassword)
 
 export default router;
